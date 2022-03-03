1bb01d245ebee6fe26429f02bb050ee5c27a1013
ife-dolapo <ifedolapootiko@gmail.com>
2022-03-02 23:07:48 -0500

    Updated time, added the css styling to l1 and removed the console log

--- src/app.js
+++ src/app.js
@@ -1,13 +1,46 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    hours = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
+  document.querySelector("#day-time").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+}
+
+function showCityName(event) {
+  event.preventDefault();
+  let cityTemperatureResult = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(cityTemperatureResult).then(displayTemperature);
 }
 
 let apiKey = `ae2ec4b5c918b23f97601cbc84d57ecd`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Hamilton&appid=${apiKey}&units=metric`;
+let city = document.querySelector("#citysearch").value;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather`;
 
-axios.get(apiUrl).then(displayTemperature);
+let cityNameResult = document.querySelector("#citybutton");
+cityNameResult.addEventListener("click", showCityName);
