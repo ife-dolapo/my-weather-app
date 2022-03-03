99ca717f7e5caac605d0d55afbe94e76562c9cc9
ife-dolapo <ifedolapootiko@gmail.com>
2022-03-03 15:22:06 -0500

    created the icons JS for the document

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ function formatDate(timestamp) {
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    hours = `0${minutes}`;
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
@@ -30,17 +30,23 @@ function displayTemperature(response) {
   document.querySelector("#day-time").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  let imgElement = document.querySelector("img");
+  let icons = response.data.weather[0].icon;
+  imgElement.setAttribute(
+    "src",
+    "https://openweathermap.org/img/wn/10d@2x.png"
+  );
 }
 
 function showCityName(event) {
   event.preventDefault();
-  let cityTemperatureResult = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric`;
+  let cityTemperatureResult = `${apiUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
   axios.get(cityTemperatureResult).then(displayTemperature);
 }
 
 let apiKey = `ae2ec4b5c918b23f97601cbc84d57ecd`;
-let city = document.querySelector("#citysearch").value;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather`;
+let cityName = `Hamilton`;
 
 let cityNameResult = document.querySelector("#citybutton");
 cityNameResult.addEventListener("click", showCityName);
